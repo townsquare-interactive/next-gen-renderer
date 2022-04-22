101ef065aceac62ff1feed965006b978327d6884
Josh Edwards <jedwards404@gmail.com>
2022-04-22 15:13:27 -0400

    foundations of first photo tile build

--- components/PhotoTiles3.tsx
+++ components/PhotoTiles3.tsx
@@ -1,21 +1,34 @@
 import styles from './photoTiles3.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const PhotoTiles3 = () => {
     return (
         <div className={styles.root}>
             <div className={styles.wrapper}>
                 <div className={`${styles.tileImage} ${styles.mainImage}`}>
-                    {' '}
-                    <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                    <Link href="/blog">
+                        <a>
+                            <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                            <div className={styles.text}>Blog</div>
+                        </a>
+                    </Link>
                 </div>
                 <div className={`${styles.tileImage} ${styles.thirdImage}`}>
-                    {' '}
-                    <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                    <Link href="/gallery">
+                        <a>
+                            <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                            <div className={styles.text}>Gallery</div>
+                        </a>
+                    </Link>
                 </div>
                 <div className={`${styles.tileImage} ${styles.thirdImage}`}>
-                    {' '}
-                    <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                    <Link href="/menu">
+                        <a>
+                            <Image src="https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg" layout="fill" objectFit="cover" />
+                            <div className={styles.text}>Menu</div>
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
