dfd2276f02a287879182f1c34bb8dda00ef6fa0d
Josh Edwards <jedwards404@gmail.com>
2022-05-01 19:41:15 -0400

    More filename changes

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,6 +1,6 @@
 import styles from './features.module.scss'
 import Card from './Card'
-import Article from './List'
+import List from './List'
 import menuData from './menu.json'
 const menu = menuData.menu
 
@@ -9,7 +9,7 @@ function Features() {
         <div className={styles.root}>
             <div className={styles.wrapper}>
                 {menu.map((item, index) => {
-                    return <Article imageUrl={item.imgSrc} title={item.title} key={index} body={item.text} modLayout="card" border={false} />
+                    return <List imageUrl={item.imgSrc} title={item.title} key={index} body={item.text} modLayout="card" border={false} />
                 })}
             </div>
         </div>
