4a045bfc5e1ba3ba78df169f21c8c2ca4fcbb25e
Josh Edwards <jedwards404@gmail.com>
2022-06-10 13:52:06 -0400

    hiding seo for now

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -95,14 +95,14 @@ const Slug = (props: HomeProps) => {
         <div>
             {page.seo && (
                 <Head>
-                    <title>{page.seo.title || 'title'}</title>
+                    {/*  <title>{page.seo.title || 'title'}</title>
                     <meta property="og:title" content={page.seo.title || 'title'} key="title" />
                     <meta name="description" content={page.seo.description || 'description'} />
                     <meta property="og:image" content={page.seo.ogImage || ''} />
                     <meta property="og:image:type" content="image/jpg" />
                     <meta property="og:image:width" content="1024" />
                     <meta property="og:image:height" content="1024" />
-                    <link rel="shortcut icon" href={page.seo.favicon || ''} />
+                    <link rel="shortcut icon" href={page.seo.favicon || ''} /> */}
                 </Head>
             )}
 
