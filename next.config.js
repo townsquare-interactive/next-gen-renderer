4484323e8cb141d8781303274d74552f3904cf17
Josh Edwards <jedwards404@gmail.com>
2022-06-14 13:55:55 -0400

    adding lang in next config

--- next.config.js
+++ next.config.js
@@ -10,15 +10,15 @@ const nextConfig = {
     },
 } */
 module.exports = {
-    env: {
-        //BASE_URL: process.env.BASE_URL,
-        //BASE_URL: 'somedomain.com',
-    },
     future: {
         webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
         // Looks like backward compatibility approach.
     },
     images: {
         domains: ['via.placeholder.com', 'unsplash.com', 'images.pexels.com', 'townsquareinteractive.s3.amazonaws.com', 'jedwards4044.github.io'],
+    },
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
     },
 }
