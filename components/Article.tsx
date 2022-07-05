9a40647d40f570ad1e1d47af325904c64197d352
Josh Edwards <jedwards404@gmail.com>
2022-07-05 12:52:37 -0400

    nosizing implemented, hero implemented

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,9 +1,10 @@
 import styles from './article.module.scss'
-import { ArticleProps } from './types'
+import { ArticleProps, Media } from './types'
 import Image from 'next/image'
 import cn from 'classnames'
 import Parser from 'html-react-parser'
 import { domainImage } from '../functions'
+import { useState } from 'react'
 
 const Article = (props: ArticleProps) => {
     /*  const themeStylesObj = {
@@ -21,6 +22,16 @@ const Article = (props: ArticleProps) => {
     const gutterStyle = {
         backgroundColor: `${themeStyles['mainColor']}`,
     } */
+    const [imageHeight, setHeight] = useState(100)
+    const [imageWidth, setWidth] = useState(300)
+
+    const calcImageSize = (loadedMedia: Media) => {
+        setWidth(loadedMedia.naturalWidth)
+        setHeight(loadedMedia.naturalHeight)
+    }
+
+    //non constrained images
+    const imageNoSizings = ['no_sizing', 'no_set_height']
 
     return (
         <div
@@ -38,19 +49,26 @@ const Article = (props: ArticleProps) => {
                     [styles.landscape_4_3]: props.imgSize === 'landscape_4_3',
                     [styles.landscape_3_2]: props.imgSize === 'landscape_3_2',
                     [styles.portrait_3_4]: props.imgSize === 'portrait_3_4',
+                    [styles.portrait_2_3]: props.imgSize === 'portrait_2_3',
+                    [styles.widescreen_16_9]: props.imgSize === 'widescreen_16_9',
+                    [styles.widescreen_3_1]: props.imgSize === 'widescreen_3_1',
+                    [styles.widescreen_2_4_1]: props.imgSize === 'widescreen_2_4_1',
+                    [styles.no_sizing]: props.imgSize === 'no_sizing',
+                    [styles.no_set_height]: props.imgSize === 'no_set_height',
                 }
             )}
         >
             {props.items.map((item, index) => (
                 <div
-                    className={cn(styles['the_list_item'], 'item_1', 'the_list_item', 'not_hero', 'yes_image', 'yes_heads', 'yes_desc', {
+                    className={cn(styles['the_list_item'], 'item_1', 'the_list_item', 'yes_image', 'yes_heads', 'yes_desc', {
                         [styles.the_list_item_right]: item.align === 'right',
                         [styles.the_list_item_left]: item.align === 'left',
+                        [styles.hero]: item.isFeatured === 'active',
                     })}
                     lang="en"
                     key={index}
                 >
-                    <div className="the_list_wrap">
+                    <div className={styles.the_list_wrap}>
                         {item.image && (
                             <div
                                 className={cn(styles['the_list_item_image'], {
@@ -68,20 +86,32 @@ const Article = (props: ArticleProps) => {
                                     src="/files/2019/03/1553524133677_pasta_with_meatballs_and_parsl_57257360.jpg?w=1440&amp;h=1080&amp;a=t"
                                      style="display: inline;" 
                                 /> */}
-
-                                    <Image
-                                        className={cn(styles['item_image'], 'item_image', 'beacon-lazy-load')}
-                                        src={domainImage(item.image)}
-                                        layout="fill"
-                                        alt="unsure"
-                                        objectFit="cover"
-                                    />
+                                    {/* {props.imgSize != 'nosizing' ? ( */}
+                                    {!imageNoSizings.includes(props.imgSize) ? (
+                                        <Image
+                                            className={cn(styles['item_image'], 'item_image', 'beacon-lazy-load')}
+                                            src={domainImage(item.image)}
+                                            layout="fill"
+                                            alt="unsure"
+                                            objectFit="cover"
+                                        />
+                                    ) : (
+                                        //Setting width and height to image props if nosizing added
+                                        <Image
+                                            src={domainImage(item.image)}
+                                            alt="logo"
+                                            onLoadingComplete={calcImageSize}
+                                            width={imageWidth}
+                                            height={imageHeight}
+                                            layout="responsive"
+                                        />
+                                    )}
                                 </a>
                             </div>
                         )}
                         <div className="the_list_item_heads">
-                            <h1 className={cn(styles['the_list_item_headline'], 'hds_color')}>{item.headline}</h1>
-                            <h3 className={cn(styles['the_list_item_subheadline'], 'hds_color')}>{item.subheader}</h3>
+                            <h1 className={cn(styles['the_list_item_headline'], 'hds_color')}>{Parser(item.headline)}</h1>
+                            <h3 className={cn(styles['the_list_item_subheadline'], 'hds_color')}>{Parser(item.subheader)}</h3>
                         </div>
                         <div className={cn(styles['the_list_item_desc'], 'txt_color')}>
                             <p>{item.desc}</p>
---
+++ stories/Article.stories.tsx
@@ -0,0 +1,181 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Article from '../components/Article'
+
+export default {
+    title: 'CMS/Article',
+    component: Article,
+    argTypes: {
+        imgSize: {
+            options: ['landscape_4_3', 'landscape_3_2', 'portrait_3_4', 'portrait_2_3', 'square_1_1', 'widescreen_16_9', 'widescreen_2_4_1', 'widescreen_3_1'],
+            control: { type: 'radio' },
+        },
+        items: {
+            align: {
+                options: ['left', 'right', 'center'],
+                control: { type: 'radio' },
+            },
+        },
+    },
+} as ComponentMeta<typeof Article>
+
+const Template: ComponentStory<typeof Article> = (args) => <Article {...args} />
+
+const Default = Template.bind({})
+Default.args = {
+    uid: '1',
+    type: 'article',
+    well: '',
+    align: 'left',
+    imgSize: 'square_4_3',
+    items: [
+        {
+            id: '6574f9b4_f1d5_4b10_feb6_928e62803df3',
+            headline: 'Article Module',
+            subheader: 'Subheader',
+            image: '/football-2.jpg',
+            captionOn: 0,
+            icon: '',
+            icon2: '',
+            icon3: '',
+            bkgrd_color: '',
+            btnType: '',
+            btnType2: '',
+            btnSize: '',
+            btnSize2: '',
+            desc: 'This is the desc',
+            pagelink: '',
+            weblink: '',
+            actionlbl: '',
+            newwindow: '',
+            pagelink2: '',
+            weblink2: '',
+            actionlbl2: '',
+            newwindow2: '',
+            align: 'center',
+            isFeatured: '',
+            isPlugin: '',
+            headerTag: '',
+            plugin: '',
+            disabled: '',
+        },
+    ],
+}
+
+export const Plain = Template.bind({})
+Plain.args = {
+    ...Default.args,
+}
+export const AlignLeft = Template.bind({})
+AlignLeft.args = {
+    ...Default.args,
+    items: [
+        {
+            id: '6574f9b4_f1d5_4b10_feb6_928e62803df3',
+            headline: 'Aligned Left',
+            subheader: 'Subheader',
+            image: '/football-2.jpg',
+            captionOn: 0,
+            icon: '',
+            icon2: '',
+            icon3: '',
+            bkgrd_color: '',
+            btnType: '',
+            btnType2: '',
+            btnSize: '',
+            btnSize2: '',
+            desc: 'This is the desc',
+            pagelink: '',
+            weblink: '',
+            actionlbl: '',
+            newwindow: '',
+            pagelink2: '',
+            weblink2: '',
+            actionlbl2: '',
+            newwindow2: '',
+            align: 'left',
+            isFeatured: '',
+            isPlugin: '',
+            headerTag: '',
+            plugin: '',
+            disabled: '',
+        },
+    ],
+}
+export const AlignRight = Template.bind({})
+AlignRight.args = {
+    ...Default.args,
+    items: [
+        {
+            id: '6574f9b4_f1d5_4b10_feb6_928e62803df3',
+            headline: 'Image square_1_1',
+            subheader: 'Subheader',
+            image: '/football-2.jpg',
+            captionOn: 0,
+            icon: '',
+            icon2: '',
+            icon3: '',
+            bkgrd_color: '',
+            btnType: '',
+            btnType2: '',
+            btnSize: '',
+            btnSize2: '',
+            desc: 'This is the desc',
+            pagelink: '',
+            weblink: '',
+            actionlbl: '',
+            newwindow: '',
+            pagelink2: '',
+            weblink2: '',
+            actionlbl2: '',
+            newwindow2: '',
+            align: 'right',
+            isFeatured: '',
+            isPlugin: '',
+            headerTag: '',
+            plugin: '',
+            disabled: '',
+        },
+    ],
+}
+export const ImageSquare = Template.bind({})
+ImageSquare.args = {
+    ...Default.args,
+    imgSize: 'square_1_1',
+}
+export const ImageLandscape_4_3 = Template.bind({})
+ImageLandscape_4_3.args = {
+    ...Default.args,
+    imgSize: 'landscape_4_3',
+}
+
+export const ImageLandscape_3_2 = Template.bind({})
+ImageLandscape_3_2.args = {
+    ...Default.args,
+    imgSize: 'landscape_3_2',
+}
+export const ImagePortrait_3_4 = Template.bind({})
+ImagePortrait_3_4.args = {
+    ...Default.args,
+    imgSize: 'portrait_3_4',
+}
+export const ImagePortrait_2_3 = Template.bind({})
+ImagePortrait_2_3.args = {
+    ...Default.args,
+    imgSize: 'portrait_2_3',
+}
+export const ImageWideScreen_16_9 = Template.bind({})
+ImageWideScreen_16_9.args = {
+    ...Default.args,
+    imgSize: 'widescreen_16_9',
+}
+
+export const ImageWideScreen_2_4_1 = Template.bind({})
+ImageWideScreen_2_4_1.args = {
+    ...Default.args,
+    imgSize: 'widescreen_2_4_1',
+}
+export const ImageWideScreen_3_1 = Template.bind({})
+ImageWideScreen_3_1.args = {
+    ...Default.args,
+    imgSize: 'widescreen_3_1',
+}
