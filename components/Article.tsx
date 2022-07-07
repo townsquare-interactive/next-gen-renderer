5fb598a540226db106c27ca1d5d90e57a092ca5a
Josh Edwards <jedwards404@gmail.com>
2022-07-07 12:52:18 -0400

    fixing storybook items

--- components/Article.tsx
+++ components/Article.tsx
@@ -8,6 +8,14 @@ import { useState } from 'react'
 import Link from 'next/link'
 
 const Article = (props: ArticleProps) => {
+    const [imageHeight, setHeight] = useState(100)
+    const [imageWidth, setWidth] = useState(300)
+
+    const calcImageSize = (loadedMedia: Media) => {
+        setWidth(loadedMedia.naturalWidth)
+        setHeight(loadedMedia.naturalHeight)
+    }
+
     //Defining style objects
     const textColor = {
         color: props.themeStyles['textColor'],
@@ -28,13 +36,9 @@ const Article = (props: ArticleProps) => {
     const borderBackground = {
         backgroundColor: props.themeStyles['headerBackground'],
     }
-
-    const [imageHeight, setHeight] = useState(100)
-    const [imageWidth, setWidth] = useState(300)
-
-    const calcImageSize = (loadedMedia: Media) => {
-        setWidth(loadedMedia.naturalWidth)
-        setHeight(loadedMedia.naturalHeight)
+    const accentColors = {
+        color: props.themeStyles['textColorAccent'],
+        backgroundColor: props.themeStyles['mainColor'],
     }
 
     //non constrained images
@@ -59,7 +63,6 @@ const Article = (props: ArticleProps) => {
                     [styles.widescreen_2_4_1]: props.imgSize === 'widescreen_2_4_1',
                     [styles.no_sizing]: props.imgSize === 'no_sizing',
                     [styles.no_set_height]: props.imgSize === 'no_set_height',
-                    [styles.no_set_height]: props.imgSize === 'no_set_height',
                     [styles.well]: props.well === '1',
                     [styles.not_well]: props.well === '',
                     [styles.large]: props.columns === 1,
@@ -74,46 +77,21 @@ const Article = (props: ArticleProps) => {
         >
             {props.items.map((item, index) => (
                 <div
-                    className={cn(styles['the_list_item'], 'item_1', {
+                    className={cn(styles['the_list_item'], {
                         [styles.the_list_item_right]: item.align === 'right',
                         [styles.the_list_item_left]: item.align === 'left',
                         [styles.hero]: item.isFeatured === 'active',
-                        ['hero']: item.isFeatured === 'active',
-                        [styles.not_hero]: item.isFeatured === '',
-                        [styles.yes_heads]: item.headline || item.subheader,
+                        [styles.not_hero]: !item.isFeatured,
                         [styles.yes_desc]: item.desc,
                         [styles.no_desc]: !item.desc,
-                        [styles.yes_image]: item.image,
                         [styles.no_image]: !item.image,
+                        [styles.yes_image]: item.image,
                     })}
                     lang="en"
                     key={index}
                 >
                     {/* <style>{themeStyles}</style> */}
                     <div className={styles.the_list_wrap} style={item.isFeatured ? accentBackground : props.well ? borderBackground : noBackground}>
-                        {/*  <a data-title="This is our article page : first element">
-                                    {!imageNoSizings.includes(props.imgSize) ? (
-                                        <Image
-                                            className={cn(styles['item_image'], 'item_image', 'beacon-lazy-load')}
-                                            src={domainImage(item.image)}
-                                            layout="fill"
-                                            alt="unsure"
-                                            objectFit="cover"
-                                        />
-                                    ) : (
-                                        //Setting width and height to image props if nosizing added
-                                        <Image
-                                            src={domainImage(item.image)}
-                                            alt="logo"
-                                            onLoadingComplete={calcImageSize}
-                                            width={imageWidth}
-                                            height={imageHeight}
-                                            layout="responsive"
-                                        />
-                                    )}
-
-
-                                    </a> */}
                         {item.image && (
                             <div
                                 className={cn(styles['the_list_item_image'], {
@@ -171,7 +149,7 @@ const Article = (props: ArticleProps) => {
                                 )}
                             </div>
                         )}
-                        <div className="the_list_item_heads">
+                        <div className={styles['the_list_item_heads']}>
                             <h1
                                 className={cn(styles['the_list_item_headline'], styles['hds_color'])}
                                 style={props.well || item.isFeatured ? textColorAccent : textColorHeading}
@@ -192,12 +170,7 @@ const Article = (props: ArticleProps) => {
                             <p>{Parser(item.desc)}</p>
                         </div>
                         {item.pagelink && item.actionlbl && (
-                            <TheListItemAction
-                                pagelink={item.pagelink}
-                                actionlbl={item.actionlbl}
-                                newwindow={item.newwindow}
-                                textColorAccent={textColorAccent}
-                            />
+                            <TheListItemAction pagelink={item.pagelink} actionlbl={item.actionlbl} newwindow={item.newwindow} accentColors={accentColors} />
                         )}
                     </div>
                 </div>
@@ -210,7 +183,7 @@ const TheListItemAction = (props: any) => {
     return (
         <div className={styles.the_list_item_action}>
             <Link href={props.pagelink}>
-                <a target={props.newwindow === 1 ? '_blank' : '_self'} className="accent_color_bg accent_txt_color" style={props.textColorAccent}>
+                <a target={props.newwindow === 1 ? '_blank' : '_self'} className="accent_color_bg accent_txt_color" style={props.accentColors}>
                     {props.actionlbl}
                 </a>
             </Link>
