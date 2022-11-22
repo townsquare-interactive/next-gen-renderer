51c2986dfa6700acd014e70a158fedf99b047e78
Josh Edwards <jedwards404@gmail.com>
2022-11-22 09:52:27 -0500

    feat: PhotoGrid link-block added (NGEN-4)

--- components/PhotoGrid.tsx
+++ components/PhotoGrid.tsx
@@ -5,6 +5,9 @@ import { Button } from '../elements/MyButton'
 import Parser from 'html-react-parser'
 import cn from 'classnames'
 import { MyImage } from '../elements/MyImage'
+import { ConditionalWrapper } from 'functions'
+import Link from 'next/link'
+import { ReactChild } from 'react'
 
 export const PhotoGrid = (props: ArticleProps) => {
     const { width = '1060', columns = 1, type, well, imgsize, modId, title, items, themeStyles, cmsUrl, disabled } = props
@@ -46,7 +49,7 @@ const PhotoItem = (props: PhotoItemProps) => {
     const linkAndBtn =
         (item.actionlbl && item.pagelink) || (item.actionlbl && item.weblink) || (item.actionlbl2 && item.pagelink2) || (item.actionlbl2 && item.weblink2)
 
-    function isLink() {
+    function isCap() {
         if (item.pagelink || item.pagelink2 || item.weblink || item.weblink2 || item.headline) {
             return true
         } else {
@@ -55,6 +58,28 @@ const PhotoItem = (props: PhotoItemProps) => {
     }
 
     /*     const noConstraintsCol3 = items[itemIndex - 1]?.isFeatured === 'active' || items[itemIndex - 1]?.isFeatured === 'active' */
+
+    function isButton() {
+        if (item.actionlbl || item.actionlbl2) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function isLink() {
+        if (item.pagelink || item.pagelink2 || item.weblink || item.weblink2) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const oneButton =
+        (item.actionlbl && !item.actionlbl2 && (item.pagelink || item.weblink)) || (!item.actionlbl && item.actionlbl2 && (item.pagelink2 || item.weblink2))
+    /* const twoButtons = item.actionlbl && item.actionlbl2 && (item.pagelink || item.weblink) && (item.pagelink2 || item.weblink2) */
+    const linkNoBtn = isButton() === false && isLink() === true
+    const wrapLink = (oneButton || linkNoBtn) && type != 'article'
 
     return (
         <article
@@ -82,38 +107,58 @@ const PhotoItem = (props: PhotoItemProps) => {
             lang="en"
             //style={well == '1' ? borderBackground : noBackground}
         >
-            {item.image && <MyImage item={item} well={well} imgsize={imgsize} cmsUrl={cmsUrl} />}
-            {isLink() && (
-                <figcaption className={cn(styles.caption)} style={{ background: themeStyles.captionBackground }}>
-                    <div>
-                        {item.headline && <h3 className={cn(styles['hd'], ['caption-txt'])}>{Parser(item.headline)}</h3>}
-                        {linkAndBtn && (
-                            <Button
-                                pagelink={item.pagelink}
-                                actionlbl={item.actionlbl}
-                                newwindow={item.newwindow}
-                                newwindow2={item.newwindow2}
-                                actionlbl2={item.actionlbl2}
-                                pagelink2={item.pagelink2}
-                                weblink2={item.weblink2}
-                                weblink={item.weblink}
-                                icon={item.icon}
-                                icon2={item.icon2}
-                                btnType={item.btnType}
-                                btnType2={item.btnType2}
-                                themeStyles={themeStyles}
-                                btnSize={item.btnSize}
-                                btnSize2={item.btnSize2}
-                                well={well}
-                                modId={modId}
-                                type={type}
-                                align={item.align}
-                                columns={columns}
-                            />
-                        )}
-                    </div>
-                </figcaption>
-            )}
+            <ConditionalWrapper
+                condition={wrapLink ? true : false}
+                trueOutput={(children: ReactChild) => (
+                    <Link
+                        href={item.pagelink || item.weblink || item.pagelink2 || item.weblink2 || ''}
+                        passHref={item.weblink || item.weblink2 ? true : false}
+                        className={cn('btn_link', styles['link-wrap'])}
+                        target={item.newwindow == 1 ? '_blank' : item.newwindow2 == 1 ? '_blank' : '_self'}
+                        aria-label={item.headline || 'block-link'}
+                    >
+                        <div className={cn(styles['item-wrap'], 'btn_link')} aria-label={item.headline || 'item-wrap'}>
+                            {children}
+                        </div>
+                    </Link>
+                )}
+                falseOutput={(children: ReactChild) => <div className={styles['item-wrap']}>{children}</div>}
+            >
+                <>
+                    {item.image && <MyImage item={item} well={well} imgsize={imgsize} cmsUrl={cmsUrl} />}
+                    {isCap() && (
+                        <figcaption className={cn(styles.caption)} style={{ background: themeStyles.captionBackground }}>
+                            <div>
+                                {item.headline && <h3 className={cn(styles['hd'], ['caption-txt'])}>{Parser(item.headline)}</h3>}
+                                {linkAndBtn && (
+                                    <Button
+                                        pagelink={item.pagelink}
+                                        actionlbl={item.actionlbl}
+                                        newwindow={item.newwindow}
+                                        newwindow2={item.newwindow2}
+                                        actionlbl2={item.actionlbl2}
+                                        pagelink2={item.pagelink2}
+                                        weblink2={item.weblink2}
+                                        weblink={item.weblink}
+                                        icon={item.icon}
+                                        icon2={item.icon2}
+                                        btnType={item.btnType}
+                                        btnType2={item.btnType2}
+                                        themeStyles={themeStyles}
+                                        btnSize={item.btnSize}
+                                        btnSize2={item.btnSize2}
+                                        well={well}
+                                        modId={modId}
+                                        type={type}
+                                        align={item.align}
+                                        columns={columns}
+                                    />
+                                )}
+                            </div>
+                        </figcaption>
+                    )}
+                </>
+            </ConditionalWrapper>
         </article>
     )
 }
