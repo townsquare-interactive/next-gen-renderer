a72853192796ea6ecdbb86b3a595ddca2ba0e001
Josh Edwards <jedwards404@gmail.com>
2023-01-16 15:50:21 -0500

    fix: add headline font for Module title

--- elements/ModuleTitle.tsx
+++ elements/ModuleTitle.tsx
@@ -3,7 +3,7 @@ import styles from './moduletitle.module.scss'
 
 const ModuleTitle = ({ title }: { title: string }) => {
     return (
-        <h2 className={cn(styles['root'], 'txt-color-hd')} data-title="module headline">
+        <h2 className={cn(styles['root'], 'txt-color-hd', 'hd-font')} data-title="module headline">
             <span>{title}</span>
         </h2>
     )
