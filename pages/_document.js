f2dd7a5f36c6a8a25fb5f17ef0f14a32fb083542
Josh Edwards <jedwards404@gmail.com>
2023-01-17 09:56:53 -0500

    feat: use only 1 ext stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 const cmsUrl = process.env.CMS_URL || 'clttestsiteforjoshedwards'
 const s3ColorStyles = process.env.BUCKET_URL + '/' + cmsUrl + '/' + 'global.css'
-const s3CustomStyles = process.env.BUCKET_URL + '/' + cmsUrl + '/' + 'custom.css'
 
 function Document() {
     return (
         <Html>
             <Head>
                 <link rel="stylesheet" href={s3ColorStyles} type="text/css"></link>
-                <link rel="stylesheet" href={s3CustomStyles} type="text/css"></link>
             </Head>
             <body>
                 <Main />
-                <NextScript />
+                <NextScript></NextScript>
             </body>
         </Html>
     )
