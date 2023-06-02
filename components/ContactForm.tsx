3f0322fb2e7576e8db7bfddeb9321182415ebe1e
jedwards4044 <jedwards404@gmail.com>
2023-06-02 14:27:24 -0400

    feat: use new obj for fields (ngen-36)

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,11 +2,11 @@
 'use client'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import { useRef } from 'react'
+//import { useRef, useState } from 'react'
 import styles from './contactform.module.scss'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
-const MonkeyForm = (props: any) => {
+const MailChimpForm = (props: any) => {
     const url = process.env.NEXT_PUBLIC_MAILCHIMP_SUBSCRIBE_URL || ''
     return (
         <div>
@@ -43,65 +43,18 @@ const ContactForm = ({ status, message, onValidated }: any) => {
         state &&
         email.value.indexOf('@') > -1 &&
         onValidated({
-            EMAIL: email.value,
-            FNAME: fName.value,
-            LNAME: lName.value,
-            MESSAGE: messagebox.value,
-            PHONE: phone.value,
-            STREET: street.value,
-            ZIP: zip.value,
-            CITY: city.value,
-            STATE: state.value,
-            /*  merge_fields: {
-                FNAME: fName.value,
-                PHONE: phone.value,
-                MESSAGE: messagebox.value,
-                //MESSAGE: messagebox.value,
-                //LNAME: 'mcgee',
-                              ADDRESS: {
-                    addr1: 'sdf',
-                    addr2: 'sdf',
-                    city: 'dsf',
-                    state: 'sdf',
-                    zip: '28204',
-                    country: 'US',
-                }, 
-                //PHONE: '4444444444',
-                //BIRTHDAY: '12/29/1940',
-            },*/
+            EMAIL: email.value || '',
+            FNAME: fName.value || '',
+            LNAME: lName.value || '',
+            MESSAGE: messagebox.value || '',
+            PHONE: phone.value || '',
+            STREET: street.value || '',
+            ZIP: zip.value || '',
+            CITY: city.value || '',
+            STATE: state.value || '',
         })
 
-    const nameRef = useRef(null)
-    const emailRef = useRef(null)
-
-    /* const subscribeUser = async (e) => {
-        e.preventDefault()
-
-        const res = await fetch('/api/subscribeuser', {
-            body: JSON.stringify({
-                email: emailRef.current.value,
-                name: nameRef.current.value,
-            }),
-
-            headers: {
-                'Content-Type': 'application/json',
-            },
-
-            method: 'POST',
-        })
-    } */
-    /*     const subscribeUser = async (e: any) => {
-        e.preventDefault()
-
-        try {
-            const response = await axios.post('/api/addcontact', { email })
-            console.log(response)
-        } catch (e) {
-            console.log(e)
-        }
-    } */
-
-    const formFields = [
+    /*   const formFields = [
         {
             name: 'fName',
             placeholder: 'Enter Name',
@@ -141,122 +94,145 @@ const ContactForm = ({ status, message, onValidated }: any) => {
             isReq: true,
             fieldType: 'textarea',
         },
-    ]
+    ] */
+
+    const formFieldsObj = {
+        firstName: {
+            isVisible: true,
+            label: 'First Name',
+            isReq: true,
+            classname: '',
+            type: 'text',
+            placeholder: 'first name',
+        },
+        lastName: {
+            isVisible: true,
+            label: 'Last Name',
+            isReq: true,
+            classname: '',
+            type: 'text',
+            placeholder: 'last name',
+        },
+        email: {
+            isVisible: true,
+            label: 'Email',
+            isReq: true,
+            classname: '',
+            type: 'email',
+            placeholder: '',
+        },
+        phone: {
+            isVisible: true,
+            label: 'Phone',
+            isReq: false,
+            classname: '',
+            type: 'phone',
+        },
+        message: {
+            isVisible: true,
+            label: 'Message',
+            isReq: true,
+            classname: '',
+            type: 'textarea',
+        },
+        streetAddress: {
+            isVisible: true,
+            label: 'Street Address',
+            isReq: false,
+            classname: '',
+            type: 'text',
+        },
+        zipCode: {
+            isVisible: true,
+            label: 'Zip Code',
+            isReq: false,
+            classname: '',
+            type: 'number',
+        },
+        city: {
+            isVisible: true,
+            label: 'City',
+            isReq: false,
+            classname: '',
+            type: 'text',
+        },
+        state: {
+            isVisible: true,
+            label: 'State',
+            isReq: true,
+            classname: '',
+            type: 'text',
+        },
+    }
 
     return (
         <>
-            {true ? (
-                <div className={styles.root}>
-                    <h3>Form Title</h3>
-                    {/*  <form
-                    //onSubmit={subscribeUser}
-                    //action={createPost}
-                    >
-                                          {formFields.map((field, index) => (
-                        <ContactField label={field.label} name={field.name} isReq={field.isReq} type={field.type} fieldType={field.fieldType} key={index} />
-                    ))} */}
+            <div className={styles.root}>
+                <h3>Form Title</h3>
 
-                    {/* <ContactField
-                            label={formFields[0].label}
-                            name={formFields[0].name}
-                            isReq={formFields[0].isReq}
-                            type={formFields[0].type}
-                            fieldType={formFields[0].fieldType}
-                            inputRef={nameRef}
-                        />
-
-                        <ContactField
-                            label={formFields[2].label}
-                            name={formFields[2].name}
-                            isReq={formFields[2].isReq}
-                            type={formFields[2].type}
-                            fieldType={formFields[2].fieldType}
-                            inputRef={emailRef}
-                            //onChange={(e: any) => setEmail(e.target.value)}
-                        /> */}
+                <div className={styles.message}>
                     {status === 'sending' && <div style={{ color: 'blue' }}>sending...</div>}
                     {status === 'error' && <div style={{ color: 'red' }} dangerouslySetInnerHTML={{ __html: message }} />}
                     {status === 'success' && <div style={{ color: 'green' }} dangerouslySetInnerHTML={{ __html: message }} />}
-                    <div className={styles.field}>
-                        <label>
-                            {formFields[0].label} {formFields[0].isReq && <span className={styles.req}>*</span>}
-                        </label>
-                        <input ref={(node) => (fName = node)} type="text" placeholder="First name" />
-                    </div>
-                    <div className={styles.field}>
-                        <label>
-                            {formFields[1].label} {formFields[1].isReq && <span className={styles.req}>*</span>}
-                        </label>
-                        <input ref={(node) => (lName = node)} type="text" placeholder="Last name" />
-                    </div>
-                    <div className={styles.field}>
-                        <label>
-                            {formFields[2].label} {formFields[2].isReq && <span className={styles.req}>*</span>}
-                        </label>
-                        <input ref={(node) => (email = node)} type="email" placeholder="Your email" />
-                    </div>
-                    <div className={styles.field}>
-                        <label>
-                            {formFields[3].label} {formFields[3].isReq && <span className={styles.req}>*</span>}
-                        </label>
-                        <input ref={(node) => (phone = node)} type="phone" placeholder="Your phone" />
-                    </div>
-                    <div className={styles.field}>
-                        <label>Address {formFields[4].isReq && <span className={styles.req}>*</span>}</label>
-                        <input ref={(node) => (street = node)} type="textarea" />
-                        <span>Street Address</span>
-                    </div>
-                    <div className={styles.field}>
-                        <input ref={(node) => (zip = node)} type="textarea" />
-                        <span>Zip Code</span>
-                    </div>
-                    <div className={styles.field}>
-                        <input ref={(node) => (city = node)} type="textarea" />
-                        <span>City</span>
-                    </div>
-                    <div className={styles.field}>
-                        <input ref={(node) => (state = node)} type="textarea" />
-                        <span>State</span>
-                    </div>
-                    <div className={styles.field}>
-                        <label>
-                            {formFields[4].label} {formFields[4].isReq && <span className={styles.req}>*</span>}
-                        </label>
-                        <input ref={(node) => (messagebox = node)} type="textarea" placeholder="Message" />
-                    </div>
-
-                    <button type="submit" className={styles.submit} onClick={submit}>
-                        Send
-                    </button>
-
-                    {/*  </form> */}
                 </div>
-            ) : (
-                <div
-                    style={{
-                        background: '#efefef',
-                        borderRadius: 2,
-                        padding: 10,
-                        display: 'inline-block',
-                    }}
-                >
-                    {status === 'sending' && <div style={{ color: 'blue' }}>sending...</div>}
-                    {status === 'error' && <div style={{ color: 'red' }} dangerouslySetInnerHTML={{ __html: message }} />}
-                    {status === 'success' && <div style={{ color: 'green' }} dangerouslySetInnerHTML={{ __html: message }} />}
-                    <input style={{ fontSize: '2em', padding: 5 }} ref={(node) => (fName = node)} type="text" placeholder="Your name" />
-                    <br />
-                    <input style={{ fontSize: '2em', padding: 5 }} ref={(node) => (email = node)} type="email" placeholder="Your email" />
-                    <br />
-                    <button style={{ fontSize: '2em', padding: 5 }} onClick={submit}>
-                        Submit
-                    </button>
+
+                <div className={styles.field}>
+                    <label>
+                        {formFieldsObj.firstName.label} {formFieldsObj.firstName.isReq && <span className={styles.req}>*</span>}
+                    </label>
+                    <input ref={(node) => (fName = node)} type="text" placeholder="First name" />
                 </div>
-            )}
+                <div className={styles.field}>
+                    <label>
+                        {formFieldsObj.lastName.label} {formFieldsObj.lastName.isReq && <span className={styles.req}>*</span>}
+                    </label>
+                    <input ref={(node) => (lName = node)} type="text" placeholder="Last name" />
+                </div>
+                <div className={styles.field}>
+                    <label>
+                        {formFieldsObj.email.label} {formFieldsObj.email.isReq && <span className={styles.req}>*</span>}
+                    </label>
+                    <input ref={(node) => (email = node)} type="email" placeholder="Your email" />
+                </div>
+                <div className={styles.field}>
+                    <label>
+                        {formFieldsObj.phone.label} {formFieldsObj.phone.isReq && <span className={styles.req}>*</span>}
+                    </label>
+                    <input ref={(node) => (phone = node)} type="phone" placeholder="Your phone" />
+                </div>
+                <div className={styles.field}>
+                    <label>Address {formFieldsObj.streetAddress.isReq && <span className={styles.req}>*</span>}</label>
+                    <input ref={(node) => (street = node)} type="textarea" />
+                    <span>Street Address</span>
+                </div>
+                <div className={styles.field}>
+                    <input ref={(node) => (zip = node)} type="textarea" />
+                    <span>{formFieldsObj.zipCode.label}</span>
+                </div>
+                <div className={styles.field}>
+                    <input ref={(node) => (city = node)} type="textarea" />
+                    <span>{formFieldsObj.city.label}</span>
+                </div>
+                <div className={styles.field}>
+                    <input ref={(node) => (state = node)} type="textarea" />
+                    <span>{formFieldsObj.state.label}</span>
+                </div>
+                <div className={styles.field}>
+                    <label>
+                        {formFieldsObj.message.label} {formFieldsObj.message.isReq && <span className={styles.req}>*</span>}
+                    </label>
+                    <input ref={(node) => (messagebox = node)} type="textarea" placeholder="Message" />
+                </div>
+
+                <button type="submit" className={styles.submit} onClick={submit}>
+                    Send
+                </button>
+            </div>
         </>
     )
 }
 
-const ContactField = (props: any) => {
+/* const ContactField = (props: any) => {
     const { type, label, placeholder, name, isReq, fieldType, inputRef } = props
     return (
         <div className={styles.field}>
@@ -267,26 +243,10 @@ const ContactField = (props: any) => {
             {fieldType === 'textarea' && <textarea name={name} placeholder={placeholder || ''} required={isReq || false} ref={inputRef}></textarea>}
         </div>
     )
-}
+} */
 
-/* const MailForm = (props: any) => {
-    const url = 'https://jster.us7.list-manage.com/subscribe/post?u=ed40c0084a0c5ba31b3365d65&id=ec6f32bf5e'
-    return (
-        <div>
-            <MailchimpSubscribe
-                url={url}
-                render={({ subscribe, status, message }) => <ContactForm status={status} message={message} onValidated={(formData:any) => subscribe(formData)} />}
-            />
-        </div>
-    )
-}
- */
-export default MonkeyForm
+export default MailChimpForm
 
-function onValidated(arg0: { EMAIL: any; NAME: any }) {
-    throw new Error('Function not implemented.')
-}
 /* function onValidated(arg0: { EMAIL: any; NAME: any }) {
     throw new Error('Function not implemented.')
-}
- */
+} */
