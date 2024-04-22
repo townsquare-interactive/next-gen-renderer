8a1b9b3c63e7043ea6649a72b48bc5ba59fd6120
Josh Edwards <jedwards404@gmail.com>
2024-04-22 15:23:03 -0400

    feat: upgrade to Next.js 14.2 (NGEN-175)

--- app/[domain]/head.tsx
+++ app/[domain]/head.tsx
@@ -1,6 +1,5 @@
 import PageHead from 'components/PageHead'
 import { generateLayout, getAnyPageData } from 'functions'
-import Script from 'next/script'
 
 export default async function Head({ params }: { params: { slug: string; domain: string } }) {
     const { CMSLayout } = await generateLayout(params)
