af87225d873093fa8b4bacdceb9ce7d477a1de64
Josh Edwards <jedwards404@gmail.com>
2024-07-09 12:36:18 -0400

    fix: add window check to ReturnNextScript (NGEN-229)

--- components/custom/ReturnNextScript.tsx
+++ components/custom/ReturnNextScript.tsx
@@ -1,11 +1,9 @@
-'use client'
 import Parser from 'html-react-parser'
 import dynamic from 'next/dynamic'
 import Script from 'next/script'
 import { Fragment, useEffect, useState } from 'react'
-const NoSrcScripts = dynamic(() => import('./NoSrcScripts'), {
-    ssr: false,
-})
+
+const NoSrcScripts = dynamic(() => import('./NoSrcScripts'), { ssr: false })
 
 interface ScriptObject {
     src: string
@@ -19,14 +17,12 @@ interface NoSrcObject {
     id: number
     script: string
 }
+
 interface CustomWindow extends Window {
     lcpMeasured?: boolean
     tbtMeasured?: boolean
     totalBlockingTime?: number
 }
-
-// Cast window to CustomWindow to access custom properties
-const customWindow = window as CustomWindow
 
 const ReturnNextScript = ({ code }: { code: string }) => {
     const [srcScripts, setSrcScripts] = useState<ScriptObject[]>([])
@@ -36,136 +32,101 @@ const ReturnNextScript = ({ code }: { code: string }) => {
 
     const delayScriptsAttempt = false
 
-    useEffect(() => {
-        function observeLCP(callback: (metric: string) => void) {
-            // Create a PerformanceObserver to observe LCP entries
-            const observer = new PerformanceObserver((list) => {
-                // Iterate through the entries
-                list.getEntries().forEach((entry) => {
-                    // Check if the entry is for Largest Contentful Paint
-                    if (entry.entryType === 'largest-contentful-paint') {
-                        // Call the callback function with the LCP entry
-                        callback('lcp')
-                    }
-                })
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(code, 'text/html')
+    const scriptTags = doc.querySelectorAll('script')
+    const divTagList = doc.querySelectorAll('div')
+
+    const observeLCP = (callback: (metric: string) => void) => {
+        const observer = new PerformanceObserver((list) => {
+            list.getEntries().forEach((entry) => {
+                if (entry.entryType === 'largest-contentful-paint') {
+                    callback('lcp')
+                }
             })
+        })
+        observer.observe({ type: 'largest-contentful-paint', buffered: true })
+    }
 
-            // Observe LCP entries
-            observer.observe({ type: 'largest-contentful-paint', buffered: true })
-        }
-
-        function observeTBT(callback: (metric: string, value: number) => void) {
-            let totalBlockingTime = 0
-
-            // Create a PerformanceObserver to observe Long Task entries
-            const observer = new PerformanceObserver((list) => {
-                // Iterate through the entries
-                list.getEntries().forEach((entry) => {
-                    // Check if the entry is for Long Task
-                    if (entry.entryType === 'longtask') {
-                        // Add the duration of the Long Task to Total Blocking Time
-                        totalBlockingTime += entry.duration
-                    }
-                })
+    const observeTBT = (callback: (metric: string, value: number) => void) => {
+        let totalBlockingTime = 0
+        const observer = new PerformanceObserver((list) => {
+            list.getEntries().forEach((entry) => {
+                if (entry.entryType === 'longtask') {
+                    totalBlockingTime += entry.duration
+                }
             })
+        })
+        observer.observe({ type: 'longtask', buffered: true })
+        setTimeout(() => {
+            callback('tbt', totalBlockingTime)
+        }, 1000)
+    }
 
-            // Observe Long Task entries
-            observer.observe({ type: 'longtask', buffered: true })
-
-            // Execute callback function after a short delay (adjust as needed)
-            setTimeout(() => {
-                // Call the callback function with the Total Blocking Time
-                callback('tbt', totalBlockingTime)
-            }, 1000) // Delay execution for 1 second to ensure TBT calculation
-        }
-
-        function handleObservations(metric: string, value?: number) {
+    const handleObservations = (metric: string, value?: number) => {
+        if (typeof window !== 'undefined') {
+            const customWindow = window as CustomWindow
             if (metric === 'lcp') {
-                // Set flag for LCP measurement
                 customWindow.lcpMeasured = true
-                console.log('lcp window measured')
             } else if (metric === 'tbt') {
-                console.log('tbt window measured')
-                // Set flag for TBT measurement
                 customWindow.tbtMeasured = true
-                // Store TBT value
                 customWindow.totalBlockingTime = value!
             }
-
-            // Check if both LCP and TBT have been measured
             if (customWindow.lcpMeasured && customWindow.tbtMeasured) {
-                console.log('window measured')
-                // Run script after both metrics have been measured
                 fullScriptCode()
             }
         }
+    }
 
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(code, 'text/html')
-        const scriptTags = doc.querySelectorAll('script')
-        const linkTags = doc.querySelectorAll('link[rel="stylesheet"]')
-        const divTags = doc.querySelectorAll('div')
-
-        function fullScriptCode() {
-            //load initial src scripts
-            const loadScripts = async () => {
-                const srcScriptList: ScriptObject[] = []
-                // Load each script sequentially
-                for (let i = 0; i < scriptTags.length; i++) {
-                    const script = scriptTags[i]
-                    if (script.src) {
-                        try {
-                            const scriptObj = {
-                                src: script.src,
-                                nonce: script.nonce || '',
-                                crossOrigin: script.crossOrigin || 'anonymous',
-                                dataApiKey: script.getAttribute('data-api-key') || '',
-                                id: script.id || '',
-                            }
-                            srcScriptList.push(scriptObj)
-                        } catch (error) {
-                            console.error('Error fetching script:', error)
-                        }
+    const fullScriptCode = () => {
+        const loadScripts = async () => {
+            const srcScriptList: ScriptObject[] = []
+            for (let i = 0; i < scriptTags.length; i++) {
+                const script = scriptTags[i]
+                if (script.src) {
+                    const scriptObj = {
+                        src: script.src,
+                        nonce: script.nonce || '',
+                        crossOrigin: script.crossOrigin || 'anonymous',
+                        dataApiKey: script.getAttribute('data-api-key') || '',
+                        id: script.id || '',
                     }
+                    srcScriptList.push(scriptObj)
                 }
-                setSrcScripts(srcScriptList)
             }
-
-            // Load scripts without src attributes after srcs are already added
-            loadScripts().then(() => {
-                const noSrcList: NoSrcObject[] = []
-                let nonSrcCount = 1
-                scriptTags.forEach(async (script) => {
-                    if (script.textContent && !script.src) {
-                        try {
-                            //eval(script.textContent)
-                            if (!script.textContent?.includes('jQuery') && !script.textContent?.includes('$(document)')) {
-                                noSrcList.push({ id: nonSrcCount, script: script.textContent })
-                                nonSrcCount += 1
-                            }
-                        } catch (error) {
-                            console.error('Error fetching non src:', error)
-                        }
-                    }
-                })
-                setScriptsReady(true)
-                setNoSrc(noSrcList)
-            })
-
-            //create an array of div tags so they can be added below
-            const allDivs: string[] = []
-            divTags.forEach(async (divTag) => {
-                allDivs.push(divTag.outerHTML)
-            })
-            setDivTags(allDivs)
+            setSrcScripts(srcScriptList)
         }
 
-        //options for observing LCP and TBT before using scripts
-        if (delayScriptsAttempt) {
-            observeLCP(handleObservations)
-            observeTBT(handleObservations)
-        } else {
-            fullScriptCode()
+        loadScripts().then(() => {
+            const noSrcList: NoSrcObject[] = []
+            let nonSrcCount = 1
+            scriptTags.forEach((script) => {
+                if (script.textContent && !script.src) {
+                    if (!script.textContent?.includes('jQuery') && !script.textContent?.includes('$(document)')) {
+                        noSrcList.push({ id: nonSrcCount, script: script.textContent })
+                        nonSrcCount += 1
+                    }
+                }
+            })
+            setScriptsReady(true)
+            setNoSrc(noSrcList)
+        })
+
+        const allDivs: string[] = []
+        divTagList.forEach((divTag) => {
+            allDivs.push(divTag.outerHTML)
+        })
+        setDivTags(allDivs)
+    }
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            if (delayScriptsAttempt) {
+                observeLCP(handleObservations)
+                observeTBT(handleObservations)
+            } else {
+                fullScriptCode()
+            }
         }
     }, [code])
 
@@ -182,4 +143,5 @@ const ReturnNextScript = ({ code }: { code: string }) => {
         </>
     )
 }
+
 export default ReturnNextScript
